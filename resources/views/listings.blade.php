51d253a06521f71d2e0fc530126e85753b95b011
Vedran <vedran.bre@gmail.com>
2023-05-16 23:52:13 +0200

    Init

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,8 @@
+@unless (count($listings) == 0)
+    @foreach ($listings as $listing)
+        <h2>{{$listing['title']}}</h2>
+        <p>{{$listing['description']}}</p>
+    @endforeach
+    @else 
+    <p>No listings found<p>
+    @endunless
